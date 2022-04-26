857c84c12f16275492d2234a46e3d84ad8c0e79d
SU-taoyuan <shengueiwengtw@gmail.com>
2022-04-26 16:55:02 +0800

    2022/04/26 - add new house working in progress

--- src/main/java/com/grp4/houseship/house/controller/HouseUserInterfaceController.java
+++ src/main/java/com/grp4/houseship/house/controller/HouseUserInterfaceController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,11 +16,19 @@ public class HouseUserInterfaceController {
     @Autowired
     private HouseService houseService;
 
-    @GetMapping(path = "house-details")
-    public String houseDetails(Model model) {
-        HouseInfo houseInfo = houseService.searchById(2);
+    @GetMapping(path = "/housedetails/{houseid}")
+    public String houseDetails(@PathVariable("houseid") int houseid, Model model) {
+        HouseInfo houseInfo = houseService.searchById(houseid);
         model.addAttribute("houseInfo", houseInfo);
         return "/ui/house/house-details";
+    }
+
+    @GetMapping(path = "/host/addnewhouse")
+    public String addNewhouse(Model model) {
+        HouseInfo houseInfo = new HouseInfo();
+        houseInfo.setH_type(1);
+        model.addAttribute("houseInfo", houseInfo);
+        return "/ui/house/add-new-house";
     }
 
 
